526197ad1204f2e815980e0a778f32ba3b01a876
4aiman <4aiman@inbox.ru>
2018-08-27 22:11:09 +0300

    Update SDYoutubeThumbnails.user.js

--- SDYoutubeThumbnails.user.js
+++ SDYoutubeThumbnails.user.js
@@ -3,34 +3,40 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       4aiman
 // @match        https://www.youtube.com/edit*
+// @match        https://i.ytimg.com/*
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
 // @grant GM_addStyle
 // ==/UserScript==
+
+function imageExists(image_url){
+    $.ajax({
+        url:image_url,
+        type:'HEAD',
+        error: function(){
+            return false;
+        },
+        success: function(){
+            return true;
+        }
+    });
+}
+
 function IsImageOk(img) {
-    // During the onload event, IE correctly identifies any images that
-    // weren’t downloaded as not complete. Others should too. Gecko-based
-    // browsers act like NS4 in that they report this incorrectly.
-    if (!img.complete) {
-        return false;
-    }
+    if (!imageExists(img.src)) { return false; }
+    if (!img.complete) { return false; }
+    if (img.naturalWidth === 0) { return false; }
 
-    // However, they do have two very useful properties: naturalWidth and
-    // naturalHeight. These give the true size of the image. If it failed
-    // to load, either of these should be zero.
-    if (img.naturalWidth === 0) {
-        return false;
-    }
-
-    // No other way of checking: assume it’s ok.
     return true;
 }
 
 (function() {
     'use strict';
 
+
     // Your code here...
-    var resize = false;
+    var resize = true;
 
     var divs = document.getElementsByClassName("yt-thumb-clip");
     var i;
@@ -40,58 +46,53 @@ function IsImageOk(img) {
         var sub = "1.jpg";
         if (src.indexOf(sub) !== -1){
             src = src.replace(sub, "sd1.jpg");
+            img.style.width='400px';
             if (IsImageOk(img)) {
                img.src = src;
-               img.style.width='400px';
-               resize = true;
             }
         }
         sub = "2.jpg";
         if (src.indexOf(sub) !== -1){
             src = src.replace(sub, "sd2.jpg");
+            img.style.width='400px';
             if (IsImageOk(img)) {
                img.src = src;
-               img.style.width='400px';
-               resize = true;
             }
         }
         sub = "3.jpg";
         if (src.indexOf(sub) !== -1){
             src = src.replace(sub, "sd3.jpg");
+            img.style.width='400px';
             if (IsImageOk(img)) {
                img.src = src;
-               img.style.width='400px';
-               resize = true;
             }
         }
     }
 
-    if (resize) {
-        var wraps = document.getElementsByClassName(".metadata-thumbnail-chooser.vertical-thumbnail-chooser");
-        var j;
-        for (j = 0; j < wraps.length; j++) {
-            wraps[j].style.width = "420px";
-        }
-
-        wraps = document.getElementsByClassName(".selectable-thumb.standard-thumb.thumb_dimensions.selectable-thumb-selected");
-        for (j = 0; j < wraps.length; j++) {
-            wraps[j].style.width = "420px";
-        }
-
-        wraps = document.getElementsByClassName("yt-thumb-120");
-        for (j = 0; j < wraps.length; j++) {
-            wraps[j].style.width = "420px";
-        }
-
-        var info = document.getElementById('video-info')
-        info.style = 'clear: left; margin-top: -350pt';
-        GM_addStyle('dt, dd { font-size: 12pt!important; width: 125pt!important;}');
-        GM_addStyle('dl {width: 450pt!important;}');
-        GM_addStyle('h2 {font-size: 14pt!important;}');
-        GM_addStyle('.vm-sprite.yt-sprite {float:none!important;}');
-        GM_addStyle('.yt-uix-button.yt-uix-button-size-default.yt-uix-button-default.custom-thumb-button {width:420px!important; height:40pt!important; font-size:14pt!important;}');
-        GM_addStyle('.yt-uix-hovercard-target.custom-thumb-text { font-size:14pt!important;}');
-
+    var wraps = document.getElementsByClassName(".metadata-thumbnail-chooser.vertical-thumbnail-chooser");
+    var j;
+    for (j = 0; j < wraps.length; j++) {
+        wraps[j].style.width = "420px";
     }
+
+    wraps = document.getElementsByClassName(".selectable-thumb.standard-thumb.thumb_dimensions.selectable-thumb-selected");
+    for (j = 0; j < wraps.length; j++) {
+        wraps[j].style.width = "420px";
+    }
+
+    wraps = document.getElementsByClassName("yt-thumb-120");
+    for (j = 0; j < wraps.length; j++) {
+        wraps[j].style.width = "420px";
+    }
+
+    var info = document.getElementById('video-info')
+    info.style = 'clear: left; margin-top: -350pt';
+    GM_addStyle('dt, dd { font-size: 12pt!important; width: 125pt!important;}');
+    GM_addStyle('dl {width: 450pt!important;}');
+    GM_addStyle('h2 {font-size: 14pt!important;}');
+    GM_addStyle('.vm-sprite.yt-sprite {float:none!important;}');
+    GM_addStyle('.yt-uix-button.yt-uix-button-size-default.yt-uix-button-default.custom-thumb-button {width:420px!important; height:40pt!important; font-size:14pt!important;}');
+    GM_addStyle('.yt-uix-hovercard-target.custom-thumb-text { font-size:14pt!important;}');
+
 })();
 
